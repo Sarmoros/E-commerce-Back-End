da2089f51ead20e0a6e365dd06a067f524401fcc
Sarahy <sarahym0703@gmail.com>
2024-02-14 13:03:36 -0500

    added the delete route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -63,10 +63,10 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
-    const categoryData = Category.destroy({
+    const categoryData = await Category.destroy({
       where: {
         id: req.params.id,
       },
@@ -75,7 +75,8 @@ router.delete('/:id', (req, res) => {
       res.status(404).json({ message: 'No category found with that id!' });
       return;
     }
-    res.status(200).json(categoryData);
+    res.status(200).json({message: "Category deleted!"});
+
   } catch (err) {
     res.status(500).json(err);
   }
